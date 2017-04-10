4dbf8b1bcc3054f875ff4f3edd6fcb8ec923ed2f
salonikrishnan <salonikrishnan@gmail.com>
2017-04-10 10:23:24 +0100

    adding outlier removal

--- Lewandowsky.R
+++ Lewandowsky.R
@@ -4,3 +4,16 @@ head(df)
 
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
+plot(df$age,df$conspiracist_avg)
+
+max_age=120
+min_age=18
+
+df=subset(df,age>min_age&age<max_age)
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
+
+plot(df$age,df$conspiracist_avg)
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
